bd0d3389ee7158c15537a443019b665c28f6791e
Bruce <111607498+Fanarrow@users.noreply.github.com>
2023-01-12 02:46:27 -0300

    Add files via upload

---
+++ aleatory.sh
@@ -0,0 +1,10 @@
+clear
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+WHITE='\033[1;37m'
+echo ${YELLOW}'INSTALANDO O ALEATORY BOT'
+echo  ${WHITE}
+
+git clone https://github.com/NuevaGeneracionALB/aleatory-md.git
+cd aleatory-md && yarn install && npm install && npm start
---
+++ main.sh
@@ -0,0 +1,2 @@
+!bin/bash
+cd Pasta && yarn start
