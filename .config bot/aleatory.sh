7a0e0fdae59759faa5f4e26004c894027b63c393
Bruce <111607498+Fanarrow@users.noreply.github.com>
2023-03-11 22:14:19 -0300

    Update aleatory.sh

--- .config bot/aleatory.sh
+++ .config bot/aleatory.sh
@@ -1,9 +1,7 @@
 #!bin/bash
+
 clear
-GREEN='\033[0;32m'
-CYAN='\033[0;36m'
-YELLOW='\033[0;33m'
-WHITE='\033[1;37m'
-echo ${YELLOW}'INSTALANDO O ALEATORY BOT'
-echo  ${WHITE}
-git clone https://github.com/NuevaGeneracionALB/aleatory-md.git && cd aleatory-md && npm install
+
+echo 'INSTALANDO O ALEATORY BOT'
+echo
+git clone https://github.com/NuevaGeneracionALB/aleatory-md.git
