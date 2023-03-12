5214688fad37b30b0b24497e5fbb1c210ac5ff0f
Bruce <111607498+Fanarrow@users.noreply.github.com>
2023-03-12 00:42:20 -0300

    Add files via upload

--- .config bot/aleatory.sh
+++ .config bot/aleatory.sh
@@ -1,7 +1,3 @@
-#!bin/bash
-
-clear
-
-echo 'INSTALANDO O ALEATORY BOT'
-echo
-git clone https://github.com/NuevaGeneracionALB/aleatory-md.git
+z="
+";Rz='nera';Az='echo';Uz='alea';Iz='git ';Bz=' '\''IN';Wz='-md.';Sz='cion';Yz='clea';Mz='//gi';Nz='thub';Jz='clon';Cz='STAL';Fz='LEAT';Zz='r';Oz='.com';Pz='/Nue';Xz='git';Lz='tps:';Qz='vaGe';Hz='BOT'\''';Vz='tory';Kz='e ht';Tz='ALB/';Ez=' O A';Dz='ANDO';Gz='ORY ';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$z$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$z$Yz$Zz"
--- .config bot/sakura.sh
+++ .config bot/sakura.sh
@@ -1,7 +1,3 @@
-#!bin/bash
-
-clear
-
-echo 'INSTALANDO O SAKURA BOT'
-echo
-git clone https://github.com/Scheyot2/sakura-botv6.git
+z="
+";Fz='AKUR';Pz='/Sch';Sz='kura';Dz='ANDO';Tz='-bot';Uz='v6.g';Vz='it';Oz='.com';Gz='A BO';Wz='clea';Jz='clon';Xz='r';Mz='//gi';Kz='e ht';Cz='STAL';Iz='git ';Hz='T'\''';Lz='tps:';Bz=' '\''IN';Nz='thub';Rz='2/sa';Az='echo';Ez=' O S';Qz='eyot';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$z$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$z$Wz$Xz"
