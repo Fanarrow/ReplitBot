4a9194b78e642a896b01865169ce953d8dfc85c3
Bruce <111607498+Fanarrow@users.noreply.github.com>
2023-03-11 22:15:51 -0300

    Update sakura.sh

--- .config bot/sakura.sh
+++ .config bot/sakura.sh
@@ -1,9 +1,7 @@
 #!bin/bash
+
 clear
-GREEN='\033[0;32m'
-CYAN='\033[0;36m'
-YELLOW='\033[0;33m'
-WHITE='\033[1;37m'
-echo ${YELLOW}'INSTALANDO O SAKURA BOT'
-echo  ${WHITE}
-git clone https://github.com/Scheyot2/sakura-botv6.git && cd sakura-botv6 && npm install
+
+echo 'INSTALANDO O SAKURA BOT'
+echo
+git clone https://github.com/Scheyot2/sakura-botv6.git
